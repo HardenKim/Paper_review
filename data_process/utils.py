00948e0fc14f26bdb3f3129d224d02c82a1e8b56
hardenkim <tazzang0921@gmail.com>
2021-11-18 21:38:24 +0900

    data_process

---
+++ data_process/utils.py
@@ -0,0 +1,8 @@
+def sparseFeature(feature, feature_num):
+    """
+    create dictionary for sparse feature
+    :param feature_name: feature name
+    :param feature_num: the total number of sparse features that do not repeat
+    :return:
+    """
+    return {'feature_name': feature, 'feature_num': feature_num}
